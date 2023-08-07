20120e088daf99dd36e13347d9b8cf82b7e4bf87
rayen hedfi <rayen.hedfi99@gmail.com>
2023-08-07 03:43:18 -0700

    testcommit

---
+++ src/test/java/tn/actia/testclass/migration/RepoSvnService.java
@@ -0,0 +1,47 @@
+package tn.actia.testclass.migration;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import tn.actia.pfe.Pfe_App.REPO_SVN.Svn;
+import tn.actia.pfe.Pfe_App.REPO_SVN.SvnRepository;
+import tn.actia.pfe.Pfe_App.REPO_SVN.SvnService;
+
+import java.util.Optional;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class RepoSvnService {
+    @Mock
+    SvnRepository svnRepository;
+    @InjectMocks
+    SvnService svnService;
+
+
+    Svn s1 = new Svn(1l,"http://testreposvn1.com","reposvn1",4,10);
+    Svn s2 = new Svn(2l,"http://testreposvn2.com","reposvn2",6,15);
+
+    @Test
+    public void testRetrieveSvnRepo(){
+
+        Mockito.when(svnRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(s1));
+        Svn svn1= svnService.retriveSvnRepo(1l);
+        Assertions.assertNotNull(svn1);
+    }
+
+    @Test
+    public void createSvnRepoTest(){
+
+        Svn svn1 = new Svn(1l,"http://testreposvn1.com","reposvn1",4,10);
+        svnService.addsvnrepo(svn1);
+        verify(svnRepository,times(1)).save(svn1);
+    }
+
+
+}
